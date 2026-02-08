0de875a09d2813bea70a17e5926d58cb1c6bd6de
Louis Niklas Beer <louis.niklas.beer@gmail.com>
2026-02-08 09:53:12 +0100

    Warn user when decreasing mileage

--- app/src/main/java/com/beer/leasewithease/HomeActivity.kt
+++ app/src/main/java/com/beer/leasewithease/HomeActivity.kt
@@ -10,21 +10,52 @@ import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.combinedClickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material3.*
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.LocalContentColor
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberModalBottomSheetState
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.Size
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Outline
 import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.Shape
@@ -33,6 +64,9 @@ import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.Density
 import androidx.compose.ui.unit.LayoutDirection
@@ -45,9 +79,6 @@ import com.beer.leasewithease.ui.home.ContractViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import java.util.concurrent.TimeUnit
 import kotlinx.coroutines.launch
-import java.text.SimpleDateFormat
-import java.util.Date
-import java.util.Locale
 import kotlin.math.abs
 
 @AndroidEntryPoint
@@ -102,6 +133,8 @@ fun ContractList(contractViewModel: ContractViewModel, modifier: Modifier = Modi
     val contracts by contractViewModel.contracts.collectAsState()
     var selectedContractForEdit by remember { mutableStateOf<Contract?>(null) }
     var contractToDelete by remember { mutableStateOf<Contract?>(null) }
+    var showMileageDecreaseConfirmDialog by remember { mutableStateOf(false) }
+    var newMileageValue by remember { mutableStateOf(0) }
     val sheetState = rememberModalBottomSheetState()
     val scope = rememberCoroutineScope()
     val context = LocalContext.current
@@ -164,15 +197,23 @@ fun ContractList(contractViewModel: ContractViewModel, modifier: Modifier = Modi
                 )
                 Spacer(modifier = Modifier.height(16.dp))
                 Button(onClick = {
-                    scope.launch {
-                        try {
-                            val updatedContract = selectedContractForEdit!!.copy(vehicleMileage = mileage.toInt())
-                            contractViewModel.updateContract(updatedContract)
-                            sheetState.hide()
-                            selectedContractForEdit = null
-                            Toast.makeText(context, "Mileage updated successfully", Toast.LENGTH_SHORT).show()
-                        } catch (e: Exception) {
-                            Toast.makeText(context, "Error updating mileage: ${e.localizedMessage}", Toast.LENGTH_LONG).show()
+                    val newMileage = mileage.toIntOrNull()
+                    if (newMileage != null) {
+                        if (newMileage < selectedContractForEdit!!.vehicleMileage) {
+                            newMileageValue = newMileage
+                            showMileageDecreaseConfirmDialog = true
+                        } else {
+                            scope.launch {
+                                try {
+                                    val updatedContract = selectedContractForEdit!!.copy(vehicleMileage = newMileage)
+                                    contractViewModel.updateContract(updatedContract)
+                                    sheetState.hide()
+                                    selectedContractForEdit = null
+                                    Toast.makeText(context, "Mileage updated successfully", Toast.LENGTH_SHORT).show()
+                                } catch (e: Exception) {
+                                    Toast.makeText(context, "Error updating mileage: ${e.localizedMessage}", Toast.LENGTH_LONG).show()
+                                }
+                            }
                         }
                     }
                 }) {
@@ -181,6 +222,50 @@ fun ContractList(contractViewModel: ContractViewModel, modifier: Modifier = Modi
                 Spacer(modifier = Modifier.height(32.dp))
             }
         }
+    }
+
+    if (showMileageDecreaseConfirmDialog) {
+        AlertDialog(
+            onDismissRequest = { showMileageDecreaseConfirmDialog = false },
+            title = {
+                val annotatedString = buildAnnotatedString {
+                    append("Are you sure you want to ")
+                    pushStyle(SpanStyle(fontWeight = FontWeight.Bold))
+                    append("decrease")
+                    pop()
+                    append(" the mileage of the vehicle?")
+                }
+                Text(text = annotatedString)
+            },
+            confirmButton = {
+                Button(onClick = {
+                    showMileageDecreaseConfirmDialog = false
+                    Toast.makeText(context, "Action was cancelled", Toast.LENGTH_SHORT).show()
+                }) {
+                    Text("Cancel")
+                }
+            },
+            dismissButton = {
+                TextButton(
+                    onClick = {
+                        scope.launch {
+                            try {
+                                val updatedContract = selectedContractForEdit!!.copy(vehicleMileage = newMileageValue)
+                                contractViewModel.updateContract(updatedContract)
+                                showMileageDecreaseConfirmDialog = false
+                                sheetState.hide()
+                                selectedContractForEdit = null
+                                Toast.makeText(context, "Mileage updated successfully", Toast.LENGTH_SHORT).show()
+                            } catch (e: Exception) {
+                                Toast.makeText(context, "Error updating mileage: ${e.localizedMessage}", Toast.LENGTH_LONG).show()
+                            }
+                        }
+                    },
+                ) {
+                    Text("Decrease")
+                }
+            }
+        )
     }
 
     if (contractToDelete != null) {
